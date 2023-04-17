f1089f37459321c4d44422bc5f5da7ee34d6e604
babar <babarmalik102030@gmail.com>
2023-04-18 04:22:10 +0500

    smooth scrooling

--- content.js
+++ content.js
@@ -1,29 +1,35 @@
-let scrollTimeoutId;
-let scrolling = true;
+var intervalId;
+var speed = 1; // change this value to adjust the speed of scrolling
+var isPaused = false;
+var isScrollingUp = false;
 
-function scroll() {
-  if (!scrolling) {
-    return;
-  }
-  const distance = 500;
-  const delay = 1000;
-  const scrollHeight = document.documentElement.scrollHeight;
-  window.scrollBy(0, distance);
-  if (window.pageYOffset + window.innerHeight >= scrollHeight) {
-    window.scrollTo(0, 0);
-  }
-  scrollTimeoutId = setTimeout(scroll, delay);
+function startScrolling() {
+  intervalId = setInterval(function() {
+    if (!isPaused) {
+      var currentPosition = window.pageYOffset;
+      var maxPosition = document.body.scrollHeight - window.innerHeight;
+      if (currentPosition >= maxPosition) {
+        isScrollingUp = true;
+      } else if (currentPosition <= 0) {
+        isScrollingUp = false;
+      }
+      if (isScrollingUp) {
+        window.scrollBy(0, -speed);
+      } else {
+        window.scrollBy(0, speed);
+      }
+    }
+  }, 1);
 }
 
-document.addEventListener('keydown', function (event) {
-  if (event.key === 'q') {
-    scrolling = !scrolling;
-    if (scrolling) {
-      scroll();
-    } else {
-      clearTimeout(scrollTimeoutId);
-    }
+function stopScrolling() {
+  clearInterval(intervalId);
+}
+
+startScrolling();
+
+document.addEventListener('keydown', function(event) {
+  if (event.code === 'KeyQ') {
+    isPaused = !isPaused;
   }
 });
-
-scroll();
